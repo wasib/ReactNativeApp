cd849c832e0942fb389a62e0be3f803acb4b9cb2
wasib <mohammedwasib@gmail.com>
2018-11-30 23:13:42 +0530

    deleted everything

--- rn-cli.config.js
+++
@@ -1,4 +0,0 @@
-const getConfig = require('metro-bundler-config-yarn-workspaces')
-const options = { nodeModules: require('path').resolve(__dirname, '..', '..') }
-
-module.exports = getConfig(__dirname, options)
